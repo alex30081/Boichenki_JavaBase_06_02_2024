f97f79150b52b149e8e324d16fb3b825703a5546
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-02-18 19:16:36 +0200

    Home_Work_3

--- src/main/java/ua/hillel/boichenko/lessons/lesson3/HomeWork.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson3/HomeWork.java
@@ -1,4 +1,9 @@
 package ua.hillel.boichenko.lessons.lesson3;
 
 public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Hello GIT!!!!");
+        System.out.println("\n");
+        System.out.println("How are you?!!!!");
+    }
 }
