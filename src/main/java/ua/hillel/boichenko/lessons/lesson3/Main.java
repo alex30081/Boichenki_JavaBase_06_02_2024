72d1fb4ce35210fe5f63de153ef33363ce3b7255
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-02-18 11:22:35 +0200

    test_GIThub2

--- src/main/java/ua/hillel/boichenko/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson3/Main.java
@@ -3,6 +3,6 @@ package ua.hillel.boichenko.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT!!!!");
-
+        System.out.println("\n");
     }
 }
