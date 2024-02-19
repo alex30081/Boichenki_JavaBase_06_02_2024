09563b6001252656141c8e431d3a9397939782aa
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-02-19 23:51:25 +0200

    Homework1_Lesson4

--- src/main/java/ua/hillel/boichenko/lessons/lesson4/HomeWork.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson4/HomeWork.java
@@ -1,4 +1,19 @@
 package ua.hillel.boichenko.lessons.lesson4;
 
 public class HomeWork {
+    public static void main(String[] args) {
+        byte a=3;
+        double b=4.2;
+        double h=2.8;
+        double volume = (2 *(a * b + a * h + b *h));
+
+        double x = volume;
+        System.out.println("VolumeParallelepiped" + "=" + x);
+        double length = ( a * 4 + b * 4 + h * 4);
+        double y = length;
+        System.out.println("LengthSumEdges" + "=" + y);
+
+
+
+    }
 }
