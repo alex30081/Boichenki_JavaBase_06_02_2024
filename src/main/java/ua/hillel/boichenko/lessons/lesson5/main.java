e78b5efbbc6f529ae443c29eb1713646ae98442f
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-02-21 00:34:29 +0200

    Homework1_Lesson4

---
+++ src/main/java/ua/hillel/boichenko/lessons/lesson5/main.java
@@ -0,0 +1,20 @@
+package ua.hillel.boichenko.lessons.lesson5;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int c = 20;
+        System.out.println(a > b);
+        System.out.println(a >=b);
+        System.out.println();
+
+        System.out.println(a == b);
+        boolean result = a < b && a > c;
+        if (a > b) {
+            System.out.println("Hello!!!");
+        } else {
+            System.out.println("hi");}
+
+    }
+}
