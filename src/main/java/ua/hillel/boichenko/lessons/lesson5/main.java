dd85dd421ae3a6aeb6a288272899c68e4fd54a80
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-02-21 01:13:30 +0200

    Lesson5

--- src/main/java/ua/hillel/boichenko/lessons/lesson5/main.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson5/main.java
@@ -2,19 +2,38 @@ package ua.hillel.boichenko.lessons.lesson5;
 
 public class main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 15;
-        int c = 20;
-        System.out.println(a > b);
-        System.out.println(a >=b);
-        System.out.println();
+//        int a = 10;
+//        int b = 15;
+//        int c = 20;
+//        System.out.println(a > b);
+//        System.out.println(a >=b);
+//        System.out.println(a < b);
+//        System.out.println(a <= b);
+//        System.out.println(a == b);
+//        System.out.println(a != b);
+//
+//        System.out.println();
+//        System.out.println();
+//
+//        System.out.println(!true);
+//        System.out.println(!false);
+//        boolean result = a >b;
+//        System.out.println("result =" + result);
+        String str = "hello!";
+        String str2 = "hello!";
+        String str3 = new String(  "hello!");
+        System.out.println(str == str2);
+        System.out.println(str == str3);
+        System.out.println( str.equals(str2));
+        System.out.println(str.equals(str3));
+//        boolean result = a < b && a > c;
+//        if (a > b) {
+//            System.out.println("Hello!!!");
+//        } else {
+//            System.out.println("hi");}
+//        int c2 = ((int) (a * b));
+//        System.out.println(c2);
 
-        System.out.println(a == b);
-        boolean result = a < b && a > c;
-        if (a > b) {
-            System.out.println("Hello!!!");
-        } else {
-            System.out.println("hi");}
 
     }
 }
