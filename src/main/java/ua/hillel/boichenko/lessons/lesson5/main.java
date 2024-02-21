63e939a5208ddd775ac0afc29797ed5abd5d82ad
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-02-22 01:03:13 +0200

    Lesson5

--- src/main/java/ua/hillel/boichenko/lessons/lesson5/main.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson5/main.java
@@ -19,13 +19,24 @@ public class main {
 //        System.out.println(!false);
 //        boolean result = a >b;
 //        System.out.println("result =" + result);
-        String str = "hello!";
-        String str2 = "hello!";
-        String str3 = new String(  "hello!");
-        System.out.println(str == str2);
-        System.out.println(str == str3);
-        System.out.println( str.equals(str2));
-        System.out.println(str.equals(str3));
+//        String str = "hello!";
+//        String str2 = "hello!";
+//        String str3 = new String(  "hello!");
+//        System.out.println(str == str2);
+//        System.out.println(str == str3);
+//        System.out.println( str.equals(str2));
+//        System.out.println(str.equals(str3));
+
+        int a = 10;
+        int b = 15;
+        int c = 20;
+
+        boolean result = a < b && a < c && a > c;
+        System.out.println(result);
+        boolean result2 = a > b || c++ > ++a;
+        System.out.println(result2);
+        System.out.println("a =" +a);
+        System.out.println("c =" +c);
 //        boolean result = a < b && a > c;
 //        if (a > b) {
 //            System.out.println("Hello!!!");
