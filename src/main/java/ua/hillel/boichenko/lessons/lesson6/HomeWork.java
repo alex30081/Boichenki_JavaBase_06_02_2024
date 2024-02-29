483b55d2a813246ffad184230ad4df861f1110e3
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-02-29 23:48:48 +0200

    Lesson5

--- src/main/java/ua/hillel/boichenko/lessons/lesson6/HomeWork.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson6/HomeWork.java
@@ -5,16 +5,26 @@ import java.util.Scanner;
 public class HomeWork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            while (true) {
+        System.out.println("Please enter integer from 0 to 10.");
         int random = (int) (Math.random() * 11);
         int a = random;
         int userV = 0;
-        System.out.println("Please enter integer from 0 to 10.");
+
         if (scanner.hasNextInt()) {
             userV = scanner.nextInt();
-            if (userV >= 0 && userV <= 10) ;{
-                break;
-            }
-
+            if (userV >= 0 && userV <= 10) { break;
+            } else {
+                System.out.println("FROM 0 TO 10");
+              scanner.nextLine();
         }
+        }else {
+            System.out.println("Wrong data, try again!!!"); scanner.nextLine();
+        }
+
+
     }
+}
+}
 }
