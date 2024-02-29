8a8d68294c3a79c61eb9a46cf6a36a1da7d6fafa
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-03-01 00:40:11 +0200

    Lesson5

--- src/main/java/ua/hillel/boichenko/lessons/lesson6/HomeWork.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson6/HomeWork.java
@@ -5,34 +5,39 @@ import java.util.Scanner;
 public class HomeWork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            while (true) {
-        System.out.println("Please enter integer from 0 to 10.");
         int random = (int) (Math.random() * 11);
         int a = random;
-        int userV = 0;
-
-        if (scanner.hasNextInt()) {
-            userV = scanner.nextInt();
-            if (userV >= 0 && userV <= 10) {
-                break;
-            } else {
-                System.out.println("FROM 0 TO 10");
-              scanner.nextLine();
-        }
-        }else {
-            System.out.println("Wrong data, try again!!!");
-            scanner.nextLine();
-        } if (userV == a) {
-                    System.out.println("Good you WIN!!!");
-                } else if (userV < a) {
-                    System.out.println("The bot created > number than you entered!");
-
-                }else{
-                    System.out.println("The bot created < number than you entered!");
-
+        for (int i = 0; i < 5; i++) {
+            int userV = 0;
+            while (true) {
+                System.out.println("Please enter integer from 0 to 10.");
+                if (scanner.hasNextInt()) {
+                    userV = scanner.nextInt();
+                    if (userV >= 0 && userV <= 10) {
+                        break;
+                    } else {
+                        System.out.println("FROM 0 TO 10");
+                        scanner.nextLine();
+                    }
+                } else {
+                    System.out.println("Wrong data, try again!!!");
+                    scanner.nextLine();
+                }
 
             }
-}
-}
+            if (userV < a) {
+                System.out.println("The bot created > number than you entered!");
+            } else if (userV > a) {
+                System.out.println("The bot created < number than you entered!");
+
+            } else {
+                System.out.println("GOOD YOU WIN!!!!");{
+                    break;
+                }
+
+            }
+
+        }
+        System.out.println("You lost, the bot come up with a number = " + a);
+    }
 }
