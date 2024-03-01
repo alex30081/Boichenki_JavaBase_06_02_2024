c18b331b505deecf288a43d1bd5e861f1c5c91c1
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-03-02 01:41:58 +0200

    Lesson5

--- src/main/java/ua/hillel/boichenko/lessons/lesson7/HomeWork8.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson7/HomeWork8.java
@@ -4,9 +4,9 @@ public class HomeWork8 {
     public static void main(String[] args) {
         int counter = 0;
         for (int i = 0; i < 1000; i++) {
-            int a = (( i / 10) % 10);
-
-           if (i /10 == 4 || i % 10 == 4 || i /10 ==9 || i % 10 == 9 || a == 4 || a == 9) {
+            int a = ( i / 10) % 10;
+            int b = i /100;
+           if (i /10 == 4 || i % 10 == 4 || i /10 ==9 || i % 10 == 9 || a == 4 || a == 9 || b == 4 || b == 9) {
              continue;
            }
 //           if( counter++ == 100) {
