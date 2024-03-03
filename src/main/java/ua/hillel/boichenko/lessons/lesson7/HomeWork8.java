7d7223802383825d389728c27a893f091ded6eaa
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-03-03 17:24:56 +0200

    Lesson5

--- src/main/java/ua/hillel/boichenko/lessons/lesson7/HomeWork8.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson7/HomeWork8.java
@@ -3,26 +3,20 @@ package ua.hillel.boichenko.lessons.lesson7;
 public class HomeWork8 {
     public static void main(String[] args) {
         int counter = 0;
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; ; i++) {
+            for ( i = 0; counter <=100; i++){
+
             int a = ( i / 10) % 10;
             int b = i /100;
            if (i /10 == 4 || i % 10 == 4 || i /10 ==9 || i % 10 == 9 || a == 4 || a == 9 || b == 4 || b == 9) {
              continue;
            }
-//           if( counter++ == 100) {
-//               break;
-//           }
-            System.out.println(i);
-           counter++;
+            System.out.println("Shuttle num " + counter + " will have a number :");
+           System.out.println(i);
+            counter++;
 
         }
-        System.out.println(counter);
-
-
-
-
-
-
+        }
         }
 
     }
