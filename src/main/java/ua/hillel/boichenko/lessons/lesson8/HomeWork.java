7e0914ffe3fe6d4257209a236666a4aea75a52ad
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-03-12 01:50:01 +0200

    HomeWork

--- src/main/java/ua/hillel/boichenko/lessons/lesson8/HomeWork.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson8/HomeWork.java
@@ -9,34 +9,39 @@ public class HomeWork {
         for (int i = 0; i < lottery.length; i++) {
             lottery[i] = ((int) (Math.random() * 10));
         }
-            for (int i = 1; i < a; i++) {
-                int key = lottery[i];
-                int j = i - 1;
-                while (j >= 0 && lottery[j] > key) {
-                    lottery[j + 1] = lottery[j];
-                    j--;
-                }
-                lottery[j + 1] = key;
+        for (int i = 1; i < a; i++) {
+            int key = lottery[i];
+            int j = i - 1;
+            while (j >= 0 && lottery[j] > key) {
+                lottery[j + 1] = lottery[j];
+                j--;
             }
-        for (int i= 0; i < player.length; i++) {
+            lottery[j + 1] = key;
+        }
+        for (int i = 0; i < player.length; i++) {
             player[i] = ((int) (Math.random() * 10));
         }
-            for (int i = 1; i < b; i++) {
-                int key = player[i];
-                int j = i - 1;
-                while (j >= 0 && player[j] > key) {
-                    player[j + 1] = player[j];
-                    j--;
-                }
-                player[j + 1] = key;
+        for (int i = 1; i < b; i++) {
+            int key = player[i];
+            int j = i - 1;
+            while (j >= 0 && player[j] > key) {
+                player[j + 1] = player[j];
+                j--;
             }
+            player[j + 1] = key;
+        }
+        int sum = 0;
+        int index = 0;
         System.out.println(" Lottery numbers = " + Arrays.toString(lottery));
         System.out.println(" Player numbers =  " + Arrays.toString(player));
         for (int i = 0; i < lottery.length; i++) {
-            if (lottery[i] == player[i] ) {
-                System.out.print(i);
-            }
-            }
-     }
-}
 
+            if (lottery[i] == player[i]) {
+                sum++;
+                index = i;
+            }
+        }
+        System.out.println("Sum of coincidences = " + sum);
+        System.out.println("Index of coincidence =" +index);
+
+    }}
