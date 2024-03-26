fa12a5909cee399d4382a2c7c0d3b37e4f58fe3a
Oleksandr Boichenko <alex.homerepair.www@gmail.com>
2024-03-26 23:16:10 +0200

    HomeWork

--- src/main/java/ua/hillel/boichenko/lessons/lesson10/Main.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson10/Main.java
@@ -1,8 +1,94 @@
 package main.java.ua.hillel.boichenko.lessons.lesson10;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+//        printHeloWorld();
+//        printHeloName("Alex");
+//        printHeloName("Den");
+//        printHeloName("Oleg");
+//        printNameEndSername("Alex", "Boichenco");
+//        int a = 5;
+//        int b =10;
+//        printSum(a,b);
+//        System.out.println("a = " + a);
+//        int[] arr = {1, 2, 3, 4, 5};
+//        printAray(arr);
+//        printSumType(5,3.4,'r', "Alex", new int[]{2, 3, 5});
+//        int random = getRandomNumber();
+//        System.out.println(random);
+//        System.out.println(getRandomNumber());
+//        printSum(getRandomNumber(), getRandomNumber());// в якости зминых выкликаем инший метод
 
+//        static void printHeloWorld () {
+//            System.out.println("Hello world!!!");
+//        }
+//        static void printHeloName(String name) {
+//            System.out.println("Hello " + name);
+//        }
+//        static void printNameEndSername(String name, String sername){
+//            System.out.println("Helo " + name + " " + sername);
+//        }
+//    static void printSum (int a, int b) {
+//         a = 10;
+//        System.out.println(a + b);
+//        System.out.println(getSum(10, 10));
+//        printVarArgs(1, 3, 3, 5, 12);
+//    }
+//        static void printAray(int[] array){
+//        array[0] = 77;
+//            for (int i = 0; i < array.length; i++) {
+//                System.out.println(array[i]);
+//
+//            }
+//        }
+//    static void printSumType (int x, double y, char ch, String str, int[] array) {
+//
+//    }
+//    static  int getSum(int a, int b) {
+//        int c = 20;
+//        return getSumDemo(a, b, c);
+//    }
+//    static  int getSumDemo(int a, int b, int c){
+//        return a + b + c;
+//    }
+//    static int getRandomNumber() {
+//        int random = (int) (Math.random() * 11);
+//        return random;
+//    }
+//    static void printVarArgs(int... varArgs) {
+//        System.out.println(Arrays.toString(varArgs));
+//        System.out.println(sum(1, 2));
+//        System.out.println(sum(1, 2, 3));
+//        System.out.println(sum(1, 2, 3, 4));
+//        System.out.println(sum(1, 2, 3, 4, 5));
+//        System.out.println(sum(3, 5, 9));
+        demoReturn(2);
     }
+//    static double sum (int a, double b, int c){
+//        return a + b + c;
+//    }
+//    static int sum(int a, int b, int c, int d, int e){
+//        return a + b + c + d + e;
+//    }
+//    static int sum(int a, int b, int c, int d){
+//
+//        return a + b + c + d;
+//    }
+//    static int sum(int a, int b, int c){
+//
+//        return a + b + c;
+//    }
+//    static int sum (int a, int b){
+//        return a + b;
+//    }
+    static void demoReturn (int x){
+        if (x == 2){
+        return;
+        }
+        System.out.println("dfaesrgsergb");
+    }
+
 
 }
--- src/main/java/ua/hillel/boichenko/lessons/lesson9/HomeWork11.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson9/HomeWork11.java
@@ -1,63 +1,78 @@
 package main.java.ua.hillel.boichenko.lessons.lesson9;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HomeWork11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = getNumber(scanner);
-        int m = getNumber(scanner);
-
-        int[][] array1 = new int[n][m];
-        int[][] array2 = new int[m][n];
-
-        for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array1[i].length; j++) {
-                array1[i][j] = (int) (Math.random() * 101);
-            }
-        }
-        System.out.println("First array:");
-        printArray(array1);
-
-        for (int i = 0; i < array2.length; i++) {
-            for (int j = 0; j < array2[i].length; j++) {
-                array2[i][j] = array1[j][i];
-            }
-        }
-        System.out.println("Second array:");
-        printArray(array2);
-
-        scanner.close();
-    }
-
-    static void printArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + "\t");
-            }
-            System.out.println();
-        }
-    }
-
-    static int getNumber(Scanner scanner) {
         int temp = 0;
-        System.out.println("Please enter array length");
+        int temp2 = 0;
+        System.out.println("Please enter Int array length!");
         while (true) {
             if (scanner.hasNextInt()) {
                 temp = scanner.nextInt();
                 if (temp < 0) {
-                    System.out.println("Please enter positive number");
+                    System.out.println("Please enter positive number!");
                 } else {
                     break;
                 }
                 scanner.nextLine();
             } else {
-                System.out.println("Wrong data, try again");
+                System.out.println("Wrong data, try again!");
                 scanner.nextLine();
             }
         }
-        return temp;
+        scanner.nextLine();
+        System.out.println("Please enter Int array width!");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                temp2 = scanner.nextInt();
+                if (temp2 < 0) {
+                    System.out.println("Please enter positive number!");
+                } else {
+                    break;
+                }
+                scanner.nextLine();
+            } else {
+                System.out.println("Wrong data, try again!");
+                scanner.nextLine();
+            }
+        }
 
-        
+        int m = temp;
+        int n = temp2;
+        int[][] array = new int[n][m];
+        int[][] array2 = new int[m][n];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int)(Math.random()*11);
+            }
+
+        }
+        System.out.println("First array!");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                array2[i][j] = array[j][i];
+
+            }
+        }
+        System.out.println("Second Array!");
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                System.out.print(array2[i][j] + "\t");
+            }
+            System.out.println();
+
+        }
+
     }
 }
