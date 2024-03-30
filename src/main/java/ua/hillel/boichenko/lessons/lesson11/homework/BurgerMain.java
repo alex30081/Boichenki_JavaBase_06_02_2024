d50950ee5bd746b8271fe1a76d11148bf19fc9b3
alex30081 <159949271+alex30081@users.noreply.github.com>
2024-03-30 22:28:29 +0200

    Update BurgerMain.java

--- src/main/java/ua/hillel/boichenko/lessons/lesson11/homework/BurgerMain.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson11/homework/BurgerMain.java
@@ -1,9 +1,17 @@
 package main.java.ua.hillel.boichenko.lessons.lesson11.homework;
 
-public class BurgerMain {
-    public static void main(String[] args) {
-        Burger burger = new Burger(" Bun, ", "Meat, ", "Cheese, ", "Grenery, ",
-                "Mayonnaise","Double Meat");
+public class BurgerMain {public static void main(String[] args) {
+        Burger burger = new Burger("bun, ", 2 , " meat, ", "cheese, ", "greenery, ",
+                "mayonnaise");
+        burger.bun = "Classic bun, ";
+        burger.meat = "Beef meat, ";
+        burger.cheese = "Cream cheese, ";
+        burger.greenery = "Lettuce leaves, ";
+        burger.mayonnaise = "Classic organic mayonnaise, ";
+        burger.classicBurger();
+        burger.dietBurger();
+        burger.burgerDoubleMeat();
+
 
     }
 }
