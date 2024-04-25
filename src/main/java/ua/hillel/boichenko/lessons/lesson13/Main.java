8437ede88750a874d18b6784a08fd97859e9bf07
Boichenko Oleksandr <alex.homerepair.www@gmail.com>
2024-04-25 23:06:48 +0100

    laptop update

--- src/main/java/ua/hillel/boichenko/lessons/lesson13/Main.java
+++ src/main/java/ua/hillel/boichenko/lessons/lesson13/Main.java
@@ -2,6 +2,6 @@ package main.java.ua.hillel.boichenko.lessons.lesson13;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hih ihi hi hi");
+        System.out.println("hih ihi hi hi" + "Laptop");
     }
 }
